dd139782c55a549fb948b4b6de48dfe523fa372f
Diya Dabhi <ddk92681@gmail.com>
2026-06-11 13:34:58 +0530

    javascript

--- task20.js
+++ task20.js
@@ -1,3 +1,6 @@
+// task  1
+
+
 let studentName = "Yash";
 const collegeName = "ABC College";
 var age = 20;
@@ -6,3 +9,27 @@ var age = 20;
 console.log("Student Name:", studentName);
 console.log("College Name:", collegeName);
 console.log("Age:", age);
+
+// task  2
+
+let city = "Rajkot";
+city = "Ahmedabad";
+console.log(city);
+
+// task  3
+
+const country = "India";
+country = "USA";
+console.log(country);
+
+// task  4
+
+let a = 25;
+let b = 10;
+
+
+console.log("Addition:", a + b);
+console.log("Subtraction:", a - b);
+console.log("Multiplication:", a * b);
+console.log("Division:", a / b);
+console.log("Modulus:", a % b);
