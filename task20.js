9241c911c8bccfe6b6fdf76b738072fa686f8af2
Diya Dabhi <ddk92681@gmail.com>
2026-06-11 13:33:15 +0530

    first commit

---
+++ task20.js
@@ -0,0 +1,8 @@
+let studentName = "Yash";
+const collegeName = "ABC College";
+var age = 20;
+
+
+console.log("Student Name:", studentName);
+console.log("College Name:", collegeName);
+console.log("Age:", age);
